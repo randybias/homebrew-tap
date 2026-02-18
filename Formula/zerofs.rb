da4585c4cce52dfce2bef374f33be0f1135c230b
Randy Bias <randybias@gmail.com>
2026-02-18 14:11:08 +0100

    Fix desc lint: don't start with article

--- Formula/zerofs.rb
+++ Formula/zerofs.rb
@@ -1,5 +1,5 @@
 class Zerofs < Formula
-  desc "The Filesystem That Makes S3 your Primary Storage. 9P/NFS/NBD on top of S3"
+  desc "Filesystem that makes S3 your primary storage via 9P, NFS, and NBD"
   homepage "https://github.com/Barre/ZeroFS"
   url "https://github.com/Barre/ZeroFS/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "7fec981e9f8c2764e118bbf319a65a748789dd0f31f16ab1c062ca2039d2c619"
